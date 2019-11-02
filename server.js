b8b19b1cd482b5f5e67584b517e58d64f7372b19
Albert Stjärne <albert.stjarne@gmail.com>
2019-11-02 18:47:26 +0100

    installing mongoose, adds db file to connect to db, connecting to the db and handling unhandled promise rejections

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// route files
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 
 // load env vars config file
 dotenv.config({ path: './config/config.env' });
+
+// connect to database
+connectDB();
+
+// route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -20,7 +24,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // close server and exit process (1 = failure)
+  server.close(() => process.exit(1));
+});

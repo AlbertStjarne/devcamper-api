670dde46d65e36ba9df4ad4c618a91a07bc18ee1
Albert Stjärne <albert.stjarne@gmail.com>
2019-11-03 11:06:42 +0100

    connects POST request to the db, adds body parser

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require('../models/Bootcamp');
+
 // @desc    get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  public
@@ -17,8 +19,17 @@ exports.getBootcamp = (req, res, next) => {
 // @desc    create new bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  private
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    update bootcamp

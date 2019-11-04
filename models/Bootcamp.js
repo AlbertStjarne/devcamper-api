2693b0c7d9ebccce88f379cf8d6654c3b6388071
Albert Stjärne <albert.stjarne@gmail.com>
2019-11-04 15:33:31 +0100

    installing node-geocoder, adds geocoder utils file, geocoding and creating location fields

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -104,6 +105,26 @@ const BootcampSchema = new mongoose.Schema({
 // create bootcamp slug from the name
 BootcampSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// geocode and create location field
+BootcampSchema.pre('save', async function(next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipCode,
+    country: loc[0].countryCode,
+  };
+
+  // do not save address in db
+  this.address = undefined;
+
   next();
 });
 

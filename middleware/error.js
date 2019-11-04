93736f5c4cf9e682697a8362d66c3cac20cd6af4
Albert Stjärne <albert.stjarne@gmail.com>
2019-11-04 10:12:22 +0100

    adds custom validation for duplicate key and ValidationError

--- middleware/error.js
+++ middleware/error.js
@@ -6,12 +6,24 @@ const errorHandler = (err, req, res, next) => {
   error.message = err.message;
 
   // log to console for dev
-  console.log(err.stack.red);
+  console.log(err);
 
   // mongoose bad ObjectId
   if (err.name === 'CastError') {
     const message = `Resource not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  // mongoose duplicate key
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  // mongoose valdation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map(val => val.message);
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({

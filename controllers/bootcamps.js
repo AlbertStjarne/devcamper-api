0030141a3663ca732f5264eee826164ed2646303
Albert Stjärne <albert.stjarne@gmail.com>
2019-11-05 13:06:50 +0100

    populating bootcamp in the courses route, adds virtuals to show courses in the bootcamp route ,adds deletion of all connected courses when deleting a bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -19,7 +19,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   // create operators ($gt, $gte etc)
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
   // finding resource
-  query = Bootcamp.find(JSON.parse(queryStr));
+  query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
 
   // select fields
   if (req.query.select) {
@@ -121,13 +121,15 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
     );
   }
+
+  bootcamp.remove();
 
   res.status(200).json({ succes: true, data: {} });
 });

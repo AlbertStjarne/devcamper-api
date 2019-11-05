7daffc583511374f1fa1a041d738edca09e45fb3
Albert Stjärne <albert.stjarne@gmail.com>
2019-11-05 09:00:37 +0100

    adds selection and sorting of fields

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,10 +8,34 @@ const Bootcamp = require('../models/Bootcamp');
 // @access  public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
   let query;
-  let queryStr = JSON.stringify(req.query);
+  // copy req.query
+  const reqQuery = { ...req.query };
+  // fields to exclude
+  const removeFields = ['select', 'sort'];
+  // loop over removeFields and delete them from reqQuery
+  removeFields.forEach(param => delete reqQuery[param]);
+  // create query string
+  let queryStr = JSON.stringify(reqQuery);
+  // create operators ($gt, $gte etc)
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
-
+  // finding resource
   query = Bootcamp.find(JSON.parse(queryStr));
+
+  // select fields
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+    console.log(fields);
+  }
+  // sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  // executing query
   const bootcamps = await query;
 
   res

ed0f904c4a1cb6e3aae19bf9885a577175e35a5a
Albert Stjärne <albert.stjarne@gmail.com>
2019-11-06 15:31:17 +0100

    adds register user functionality in controller, adds password hashing with bcryptjs

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,15 @@ const User = require('../models/User');
 // @route   GET /api/v1/auth/register
 // @access  public
 exports.register = asyncHandler(async (req, res, next) => {
-  res.status(200).json({
-    success: true,
+  const { name, email, password, role } = req.body;
+
+  // create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
   });
+
+  res.status(200).json({ success: true });
 });

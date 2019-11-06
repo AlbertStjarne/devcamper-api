199f2e7f8ce9cbdafd72e6f16280abb41aadb5cf
Albert Stjärne <albert.stjarne@gmail.com>
2019-11-06 18:27:32 +0100

    adds middleware to protect routes, apply to courses, bootcamp and auth routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -68,3 +68,12 @@ const sendTokenResponse = (user, statusCode, res) => {
     .cookie('token', token, options)
     .json({ success: true, token });
 };
+
+// @desc    get current logged in user
+// @route   GET /api/v1/auth/me
+// @access  private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({ success: true, data: user });
+});

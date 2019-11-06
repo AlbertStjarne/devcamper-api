0db0a2910032f15fe5fd02d558f09ee411c5bb0a
Albert Stjärne <albert.stjarne@gmail.com>
2019-11-06 15:52:08 +0100

    adds mongoose method to create token and get token using jsonwebtoken

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Userschema = new mongoose.Schema({
   name: {
@@ -39,5 +40,12 @@ Userschema.pre('save', async function(next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+// sign JWT and return
+Userschema.methods.getSignedJwtToken = function() {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
 
 module.exports = mongoose.model('User', Userschema);

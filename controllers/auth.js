1e0d6b545c8db337732333facde91ac83d164bbd
Albert Stjärne <albert.stjarne@gmail.com>
2019-11-06 16:49:35 +0100

    installing cookie-parser, adds cookie with token in it for register and login user

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,10 +16,7 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  // create token
-  const token = user.getSignedJwtToken();
-
-  res.status(200).json({ success: true, token: token });
+  sendTokenResponse(user, 200, res);
 });
 
 // @desc    login user
@@ -47,8 +44,27 @@ exports.login = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse('Invalid credentials', 401));
   }
 
+  sendTokenResponse(user, 200, res);
+});
+
+// get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
   // create token
   const token = user.getSignedJwtToken();
 
-  res.status(200).json({ success: true, token: token });
-});
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+
+  res
+    .status(statusCode)
+    .cookie('token', token, options)
+    .json({ success: true, token });
+};

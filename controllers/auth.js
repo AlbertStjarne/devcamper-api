ec73821852dcd2aba446700dcc4bc7ebabb160e1
Albert Stjärne <albert.stjarne@gmail.com>
2019-11-08 16:24:51 +0100

    adds controller and route for updateDetails (user name and email) and updatePassword

--- controllers/auth.js
+++ controllers/auth.js
@@ -58,6 +58,40 @@ exports.getMe = asyncHandler(async (req, res, next) => {
   res.status(200).json({ success: true, data: user });
 });
 
+// @desc    update user details
+// @route   PUT /api/v1/auth/updatedetails
+// @access  private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({ success: true, data: user });
+});
+
+// @desc    update password
+// @route   PUT /api/v1/auth/updatepassword
+// @access  private
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select('+password');
+
+  // check current password
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse('Password is incorrect', 401));
+  }
+
+  user.password = req.body.newPassword;
+  await user.save();
+
+  sendTokenResponse(user, 200, res);
+});
+
 // @desc    forgot password
 // @route   POST /api/v1/auth/forgotpassword
 // @access  public

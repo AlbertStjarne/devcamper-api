d123d03d494fda6aea2b5a1d7b4bd6be3ea06b63
Albert Stjärne <albert.stjarne@gmail.com>
2019-11-09 09:45:58 +0100

    adds Review model, adds route and controller for getReviews (all reviews for all bootcamps), adds router to get reviews for a specific bootcamp

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -13,6 +13,7 @@ const Bootcamp = require('../models/Bootcamp');
 
 // include other resource routers
 const courseRouter = require('./courses');
+const reviewRouter = require('./reviews');
 
 const router = express.Router();
 
@@ -21,6 +22,7 @@ const { protect, authorize } = require('../middleware/auth');
 
 // re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
+router.use('/:bootcampId/reviews', reviewRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampInRadius);
 

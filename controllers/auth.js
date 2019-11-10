23de007b4ecde8d4ac1ce91f197806885dbe5264
Albert Stjärne <albert.stjarne@gmail.com>
2019-11-10 22:28:45 +0100

    adds logout route and controller method, adds comment to outcommented cookie code

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,6 +49,17 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// @desc    logout user / clear cookie
+// @route   GET /api/v1/auth/logout
+// @access  private
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({ success: true, data: {} });
+});
+
 // @desc    get current logged in user
 // @route   GET /api/v1/auth/me
 // @access  private
--- middleware/auth.js
+++ middleware/auth.js
@@ -9,10 +9,12 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
+    // set token from Bearer token in header
     token = req.headers.authorization.split(' ')[1];
   }
-  // else if(req.cookies.token) {
-  //   token = req.cookies.token
+  // set token from cookie
+  // else if (req.cookies.token) {
+  //   token = req.cookies.token;
   // }
 
   // make sure token exists
